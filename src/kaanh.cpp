f192c5eab33692d0e1dd7c5d8bc89e5633fd301f
KevinLiao888 <Lnch-628120@163.com>
2019-05-23 17:39:56 +0800

    model for daye

--- src/kaanh.cpp
+++ src/kaanh.cpp
@@ -313,87 +313,35 @@ namespace kaanh
     };
     auto createModelDaye(const double *robot_pm)->std::unique_ptr<aris::dynamic::Model>
     {
-        std::unique_ptr<aris::dynamic::Model> model = std::make_unique<aris::dynamic::Model>("model");
+		aris::dynamic::PumaParam param;
+		param.d1 = 0.332;
+		param.a1 = 0.088;
+		param.a2 = 0.46;
+		param.d3 = 0.0;
+		param.a3 = 0.04;
+		param.d4 = 0.43;
 
-        // 设置重力 //
-        const double gravity[6]{ 0.0,0.0,-9.8,0.0,0.0,0.0 };
-        model->environment().setGravity(gravity);
+		param.tool0_pe[2] = 0.106;
 
-        // 添加变量 //
-        model->calculator().addVariable("PI", aris::core::Matrix(PI));
+		auto model = aris::dynamic::createModelPuma(param);
+		/*
+		//根据tool0，添加一个tool1，tool1相对于tool0在x方向加上0.1m//
+		auto &tool0 = model->partPool().back().markerPool().findByName("general_motion_0_i");//获取tool0
 
-        // add part //
-        auto &p1 = model->partPool().add<Part>("L1");
-        auto &p2 = model->partPool().add<Part>("L2");
-        auto &p3 = model->partPool().add<Part>("L3");
-        auto &p4 = model->partPool().add<Part>("L4");
-        auto &p5 = model->partPool().add<Part>("L5");
-        auto &p6 = model->partPool().add<Part>("L6");
+		double pq_ee_i[7];
+		s_pm2pq(*tool0->prtPm(), pq_ee_i);
+		pq_ee_i[0] += 0.1;//在tool0的x方向加上0.1m
 
-        // add joint //
-        const double j1_pos[3]{ 0.0, 0.0, 0.237 };
-        const double j2_pos[3]{ 0.088, 0.017, 0.327, };
-        const double j3_pos[3]{ 0.088, 0.0236, 0.787 };
-        const double j4_pos[3]{ 0.127, 0.0, 0.827, };
-        const double j5_pos[3]{ 0.523, 0.036, 0.827, };
-        const double j6_pos[3]{ 0.607, 0.0, 0.827, };
+		double pm_ee_i[16];
+		s_pq2pm(pq_ee_i, pm_ee_i);
 
-        const double j1_axis[6]{ 0.0, 0.0, 1.0 };
-        const double j2_axis[6]{ 0.0, 1.0, 0.0 };
-        const double j3_axis[6]{ 0.0, 1.0, 0.0 };
-        const double j4_axis[6]{ 1.0, 0.0, 0.0 };
-        const double j5_axis[6]{ 0.0, 1.0, 0.0 };
-        const double j6_axis[6]{ 1.0, 0.0, 0.0 };
+		auto &tool1 = model->partPool().back().markerPool().add<Marker>("tool1", pm_ee_i);//添加tool1
 
-        auto &j1 = model->addRevoluteJoint(p1, model->ground(), j1_pos, j1_axis);
-        auto &j2 = model->addRevoluteJoint(p2, p1, j2_pos, j2_axis);
-        auto &j3 = model->addRevoluteJoint(p3, p2, j3_pos, j3_axis);
-        auto &j4 = model->addRevoluteJoint(p4, p3, j4_pos, j4_axis);
-        auto &j5 = model->addRevoluteJoint(p5, p4, j5_pos, j5_axis);
-        auto &j6 = model->addRevoluteJoint(p6, p5, j6_pos, j6_axis);
-
-        // add actuation //
-        auto &m1 = model->addMotion(j1);
-        auto &m2 = model->addMotion(j2);
-        auto &m3 = model->addMotion(j3);
-        auto &m4 = model->addMotion(j4);
-        auto &m5 = model->addMotion(j5);
-        auto &m6 = model->addMotion(j6);
-
-        // add ee general motion //
-        //double pq_ee_i[]{ 0.570, -0.114, 0.84, -0.0414, 0.0414, 0.706, 0.706 };		//x方向加上0.1
-        double pq_ee_i[]{ 0.566, -0.134, 0.840, -0.0405, 0.0424, 0.694, 0.718 };
-        double pm_ee_i[16];
-        double pm_ee_j[16]{ 1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1 };
-
-        s_pq2pm(pq_ee_i, pm_ee_i);
-
-        auto &makI = p6.markerPool().add<Marker>("ee_makI", pm_ee_i);
-        auto &makJ = model->ground().markerPool().add<Marker>("ee_makJ", pm_ee_j);
-        auto &ee = model->generalMotionPool().add<aris::dynamic::GeneralMotion>("ee", &makI, &makJ, false);
-
-        // change robot pose //
-        if (robot_pm)
-        {
-            p1.setPm(s_pm_dot_pm(robot_pm, *p1.pm()));
-            p2.setPm(s_pm_dot_pm(robot_pm, *p2.pm()));
-            p3.setPm(s_pm_dot_pm(robot_pm, *p3.pm()));
-            p4.setPm(s_pm_dot_pm(robot_pm, *p4.pm()));
-            p5.setPm(s_pm_dot_pm(robot_pm, *p5.pm()));
-            p6.setPm(s_pm_dot_pm(robot_pm, *p6.pm()));
-            j1.makJ().setPrtPm(s_pm_dot_pm(robot_pm, *j1.makJ().prtPm()));
-        }
-
-        // add solver
-        auto &inverse_kinematic = model->solverPool().add<aris::dynamic::PumaInverseKinematicSolver>();
-        auto &forward_kinematic = model->solverPool().add<ForwardKinematicSolver>();
-
-        inverse_kinematic.allocateMemory();
-        forward_kinematic.allocateMemory();
-
-        inverse_kinematic.setWhichRoot(8);
-
-        return model;
+		//在根据tool1位姿反解到每一个关节时，需要调用下面两行代码来实现
+		//tool1.setPm(pm_ee_i);
+		//model->generalMotionPool()[0].updMpm();
+		*/
+		return std::move(model);
     }
 
 	auto createControllerSanXiang()->std::unique_ptr<aris::control::Controller>	/*函数返回的是一个类指针，指针指向Controller,controller的类型是智能指针std::unique_ptr*/
