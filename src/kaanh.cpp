084d864540c814736c6286c5db6f3562a78641b6
KevinLiao888 <lnch-628120@163.com>
2019-05-28 10:43:07 +0800

    ATI Force

--- src/kaanh.cpp
+++ src/kaanh.cpp
@@ -3198,99 +3198,147 @@ namespace kaanh
 
 
     // ATI Force Sensor //
-    struct ATIFSParam
-    {
-        int time;
-        std::int32_t Fx,Fy,Fz,Mx,My,Mz,status_code,sample_counter;
-    };
-    auto ATIFS::prepairNrt(const std::map<std::string, std::string> &params, PlanTarget &target)->void
-        {
-            ATIFSParam param;
-            for (auto &p : params)
-            {
-                if (p.first == "time")
-                {
-                    param.time = std::stoi(p.second);
-                }
-            }
-            param.Fx = 0;
-            param.Fy = 0;
-            param.Fz = 0;
-            param.Mx = 0;
-            param.My = 0;
-            param.Mz = 0;
-            param.status_code = 0;
-            param.sample_counter = 0;
-            target.param = param;
+       struct ATIFSParam
+       {
+           int time;
+           std::int32_t Fx,Fy,Fz,Mx,My,Mz,status_code,sample_counter,controlcodes;
+           std::int32_t forceindex, torqueindex;
+           std::uint8_t forceunit, torqueunit;
+       };
+       auto ATIFS::prepairNrt(const std::map<std::string, std::string> &params, PlanTarget &target)->void
+           {
+               auto controller = dynamic_cast<aris::control::EthercatController*>(target.controller);
+               ATIFSParam param;
+               param.Fx = 0;
+               param.Fy = 0;
+               param.Fz = 0;
+               param.Mx = 0;
+               param.My = 0;
+               param.Mz = 0;
+               param.status_code = 0;
+               param.sample_counter = 0;
+               param.controlcodes = 4096;
+               //param.controlcodes = 4352;//little range
 
-        }
-    auto ATIFS::executeRT(PlanTarget &target)->int
-        {
-            auto &param = std::any_cast<ATIFSParam&>(target.param);
-            // 访问主站 //
-            auto controller = dynamic_cast<aris::control::EthercatController*>(target.controller);
+               for (auto &p : params)
+               {
+                   if (p.first == "time")
+                   {
+                       param.time = std::stoi(p.second);
+                   }
+                   else if (p.first == "controlcodes")
+                   {
+                       param.controlcodes = std::stoi(p.second);
+                   }
+               }
 
-            controller->ecSlavePool().at(6).readPdo(0x6000, 0x01, &param.Fx ,32);
-            controller->ecSlavePool().at(6).readPdo(0x6000, 0x02, &param.Fy, 32);
-            controller->ecSlavePool().at(6).readPdo(0x6000, 0x03, &param.Fz, 32);
-            controller->ecSlavePool().at(6).readPdo(0x6000, 0x04, &param.Mx, 32);
-            controller->ecSlavePool().at(6).readPdo(0x6000, 0x05, &param.My, 32);
-            controller->ecSlavePool().at(6).readPdo(0x6000, 0x06, &param.Mz, 32);
-            controller->ecSlavePool().at(6).readPdo(0x6010, 0x00, &param.status_code, 32);
-            controller->ecSlavePool().at(6).readPdo(0x6020, 0x00, &param.sample_counter, 32);
+               /*
+               controller->ecSlavePool().at(6).readSdo(0x2021, 0x2f, &param.forceunit, 8);
+               controller->ecSlavePool().at(6).readSdo(0x2021, 0x30, &param.torqueunit, 8);
+               controller->ecSlavePool().at(6).readSdo(0x2021, 0x37, &param.forceindex, 32);
+               controller->ecSlavePool().at(6).readSdo(0x2021, 0x38, &param.torqueindex, 32);
+               */
+
+               target.param = param;
+               std::fill(target.mot_options.begin(), target.mot_options.end(),
+                   Plan::NOT_CHECK_ENABLE);
+
+           }
+       auto ATIFS::executeRT(PlanTarget &target)->int
+       {
+           auto &param = std::any_cast<ATIFSParam&>(target.param);
+           // 访问主站 //
+
+           auto controller = dynamic_cast<aris::control::EthercatController*>(target.controller);
+           if (target.count == 1)
+           {
+               controller->ecSlavePool().at(6).writePdo(0x7010, 0x01, &param.controlcodes, 32);
+           }
+           controller->ecSlavePool().at(6).readPdo(0x6000, 0x01, &param.Fx ,32);
+           controller->ecSlavePool().at(6).readPdo(0x6000, 0x02, &param.Fy, 32);
+           controller->ecSlavePool().at(6).readPdo(0x6000, 0x03, &param.Fz, 32);
+           controller->ecSlavePool().at(6).readPdo(0x6000, 0x04, &param.Mx, 32);
+           controller->ecSlavePool().at(6).readPdo(0x6000, 0x05, &param.My, 32);
+           controller->ecSlavePool().at(6).readPdo(0x6000, 0x06, &param.Mz, 32);
+           controller->ecSlavePool().at(6).readPdo(0x6010, 0x00, &param.status_code, 32);
+           controller->ecSlavePool().at(6).readPdo(0x6020, 0x00, &param.sample_counter, 32);
+
+           /*
+           //print//
+           auto &cout = controller->mout();
+           if (target.count % 100 == 0)
+           {
+               cout << std::setw(6) << param.Fx << "  ";
+               cout << std::setw(6) << param.Fy << "  ";
+               cout << std::setw(6) << param.Fz << "  ";
+               cout << std::setw(6) << param.Mx << "  ";
+               cout << std::setw(6) << param.My << "  ";
+               cout << std::setw(6) << param.Mz << "  ";
+               cout << std::setw(6) << param.status_code << "  ";
+               cout << std::setw(6) << param.sample_counter << "  ";
+               cout << std::setw(6) << param.forceunit << "  ";
+               cout << std::setw(6) << param.torqueunit<< "  ";
+               cout << std::setw(6) << param.forceindex << "  ";
+               cout << std::setw(6) << param.forceindex << "  ";
+               cout << std::endl;
+               cout << "----------------------------------------------------" << std::endl;
+           }
+           */
 
 
-            double Fx = param.Fx*1000.0 / std::pow(2,32);
-            double Fy = param.Fy*1000.0 / std::pow(2,32);
-            double Fz = param.Fz*1800.0 / std::pow(2,32);
-            double Mx = param.Mx*40.0 / std::pow(2,32);
-            double My = param.My*40.0 / std::pow(2,32);
-            double Mz = param.Mz*40.0 / std::pow(2,32);
+           double Fx = param.Fx/1000000.0;
+           double Fy = param.Fy/1000000.0;
+           double Fz = param.Fz/1000000.0;
+           double Mx = param.Mx/1000000.0;
+           double My = param.My/1000000.0;
+           double Mz = param.Mz/1000000.0;
 
-            //print//
-            auto &cout = controller->mout();
-            if (target.count % 100 == 0)
-            {
-                cout << std::setw(6) << Fx << "  ";
-                cout << std::setw(6) << Fy << "  ";
-                cout << std::setw(6) << Fz << "  ";
-                cout << std::setw(6) << Mx << "  ";
-                cout << std::setw(6) << My << "  ";
-                cout << std::setw(6) << Mz << "  ";
-                cout << std::setw(6) << param.status_code << "  ";
-                cout << std::setw(6) << param.sample_counter << "  ";
-                cout << std::pow(2,32) <<" ";
-                cout << std::endl;
 
-                cout << "----------------------------------------------------" << std::endl;
-            }
+           //print//
+           auto &cout = controller->mout();
+           if (target.count % 100 == 0)
+           {
+               cout << std::setw(6) << Fx << "  ";
+               cout << std::setw(6) << Fy << "  ";
+               cout << std::setw(6) << Fz << "  ";
+               cout << std::setw(6) << Mx << "  ";
+               cout << std::setw(6) << My << "  ";
+               cout << std::setw(6) << Mz << "  ";
+               cout << std::setw(6) << param.status_code << "  ";
+               cout << std::setw(6) << param.sample_counter << "  ";
+               cout << std::endl;
+               cout << "----------------------------------------------------" << std::endl;
+           }
 
-            //log//
-            auto &lout = controller->lout();
-            {
-                lout << Fx << " ";
-                lout << Fy << " ";
-                lout << Fz << " ";
-                lout << Mx << " ";
-                lout << My << " ";
-                lout << Mz << " ";
-                lout << param.status_code << " ";
-                lout << param.sample_counter << " ";
-                lout << std::endl;
-            }
-            param.time--;
-            return param.time;
-        }
-    auto ATIFS::collectNrt(PlanTarget &target)->void {}
-    ATIFS::ATIFS(const std::string &name) :Plan(name)
-    {
-        command().loadXmlStr(
-            "<Command name=\"atifs\">"
-            "	<GroupParam>"
-            "		<Param name=\"time\" default=\"100000\"/>"
-            "	</GroupParam>"
-            "</Command>");
-    }
+           //log//
+           auto &lout = controller->lout();
+           {
+               lout << Fx << " ";
+               lout << Fy << " ";
+               lout << Fz << " ";
+               lout << Mx << " ";
+               lout << My << " ";
+               lout << Mz << " ";
+               lout << param.status_code << " ";
+               lout << param.sample_counter << " ";
+               lout << std::endl;
+           }
+
+           param.time--;
+           return param.time;
+       }
+       auto ATIFS::collectNrt(PlanTarget &target)->void {}
+       ATIFS::ATIFS(const std::string &name) :Plan(name)
+       {
+           command().loadXmlStr(
+               "<Command name=\"atifs\">"
+               "	<GroupParam>"
+               "		<Param name=\"time\" default=\"100000\"/>"
+               "		<Param name=\"controlcodes\" default=\"4096\"/>"
+               "	</GroupParam>"
+               "</Command>");
+       }
+
 
 
 	// 清理slavepool //
