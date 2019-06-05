2d04e4045499637e3ba4c7647a1f8618243ed87d
KevinLiao888 <Lnch-628120@163.com>
2019-06-05 19:24:25 +0800

    bugfix in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,15 @@ std::vector<std::vector<std::string>> plantrack(6, std::vector<std::string>());
 std::atomic_int which_di = 0;
 
 auto xmlpath = std::filesystem::absolute(".");//获取当前工程所在的路径
+auto uixmlpath = std::filesystem::absolute(".");
 const std::string xmlfile = "kaanh.xml";
 const std::string uixmlfile = "interface_kaanh.xml";
 
 int main(int argc, char *argv[])
 {
     std::cout <<"new"<<std::endl;
-
     xmlpath = xmlpath / xmlfile;
-	auto xmlpath_ui = xmlpath / uixmlfile;
+	uixmlpath = uixmlpath / uixmlfile;
     std::cout<< xmlpath <<std::endl;
 	auto&cs = aris::server::ControlServer::instance();
 	auto port = argc < 2 ? 5866 : std::stoi(argv[1]);
@@ -64,15 +64,16 @@ int main(int argc, char *argv[])
 #ifdef UINX
 	dynamic_cast<aris::control::EthercatMaster&>(cs).scan();
 	cs.saveXmlFile(xmlpath.string().c_str());
-#endif // UINX
+#endif
 
     cs.loadXmlFile(xmlpath.string().c_str());
-	cs.interfaceRoot().loadXmlFile(xmlpath_ui.string().c_str());
+	cs.interfaceRoot().loadXmlFile(uixmlpath.string().c_str());
     //cs.start();
+
 	
 	//Start Web Socket//
 	cs.startWebSock("5866");
-	
+
 	//Receive Command//
 	cs.runCmdLine();
 
