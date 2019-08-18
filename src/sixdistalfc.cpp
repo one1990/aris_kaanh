3c08c57061b2ecfcc9d90cc739cd2bc28dce159b
gkfxlt <1506518722@qq.com>
2019-08-18 20:47:16 +0800

    ForceDir Motion

--- src/sixdistalfc.cpp
+++ src/sixdistalfc.cpp
@@ -3439,11 +3439,17 @@ auto ForceDirect::executeRT(PlanTarget &target)->int
 	{
 		for (int i = 0; i < 6; ++i)
 		{
-			step_pjs[i] = target.model->motionPool()[i].mp();
             controller->motionPool().at(i).setModeOfOperation(10);	//切换到电流控制
 		}
-        for (int i = 0; i < 7; ++i)
-            PqEnd0[i] = PqEnd[i];
+		for (int i = 0; i < 7; ++i)
+		{
+			PqEnd0[i] = PqEnd[i];
+		}
+		for (int i = 0; i < 6; ++i)
+		{;
+			step_pjs[i] = PqEnd[i];
+		}
+
 
 		PqEnd0[2] = -5;
         //PqEnd0[0] = 0.398;PqEnd0[1] = 0;PqEnd0[2] = 0.6295;PqEnd0[3] = 0;PqEnd0[4] = 0.7071;PqEnd0[5] = 0;PqEnd0[6] = 0.7071;
@@ -3452,16 +3458,63 @@ auto ForceDirect::executeRT(PlanTarget &target)->int
 		target.model->generalMotionPool()[0].getMpm(begin_pm);
 	}	
 
+	static bool flag[6] = { true,true,true,true,true,true };
+	double PosLimit[6] = {  0,0.010,0,0,0,0};
+	double NegLimit[6] = { 0,-0.010,0,0,0,0};
+	static double pArc[6], vArc[6], aArc[6], vArcMax[6] = { 0.0001,0.0001,0.0001,0.0001,0.0001,0.0001 };
+	static aris::Size t_count[6] = { 0 };
+	static int CountOffsetPos[6] = { 1,1,1,1,1,1 }, CountOffsetNeg[6] = { 1,1,1,1,1,1 };
+
+	for (int i = 0;i < 6;i++)
+	{
+
+		if (flag[i])
+		{
+			if (step_pjs[i] < PosLimit[i])
+			{
+				aris::plan::moveAbsolute(target.count - CountOffsetNeg[i] + 1, 0, PosLimit[i] - begin_pjs[i], vArcMax[i] / 1000, 0.05 / 1000 / 1000, 0.05 / 1000 / 1000, pArc[i], vArc[i], aArc[i], t_count[i]);
+
+				step_pjs[i] = step_pjs[i] + vArc[i];
+			}
+			//std::cout << vArc << "  ";
+			if ((t_count[i] - (target.count - CountOffsetNeg[i] + 1)) < 0.5 && (t_count[i] - (target.count - CountOffsetNeg[i] + 1)) > -0.5)
+			{
+				CountOffsetPos[i] = target.count;
+				flag[i] = false;
+				begin_pjs[i] = target.model->motionPool()[i].mp();
+			}
 
 
-		double TransMatrix[4][4];
-		for (int i = 0;i < 4;i++)
-			for (int j = 0;j < 4;j++)
-				TransMatrix[i][j] = begin_pm[4 * i + j];
+		}
+		if (flag[i] == false)
+		{
+			if (step_pjs[i] > NegLimit[i])
+			{
+				aris::plan::moveAbsolute(target.count - CountOffsetPos[i] + 1, 0, begin_pjs[i] - NegLimit[i], vArcMax[i] / 1000, 0.05 / 1000 / 1000, 0.05 / 1000 / 1000, pArc[i], vArc[i], aArc[i], t_count[i]);
 
-		double n[3] = { TransMatrix[0][0], TransMatrix[0][1], TransMatrix[0][2] };
-		double o[3] = { TransMatrix[1][0], TransMatrix[1][1], TransMatrix[1][2] };
-		double a[3] = { TransMatrix[2][0], TransMatrix[2][1], TransMatrix[2][2] };
+				step_pjs[i] = step_pjs[i] - vArc[i];
+			}
+
+			if ((t_count[i] - (target.count - CountOffsetPos[i] + 1)) < 0.5 && (t_count[i] - (target.count - CountOffsetPos[i] + 1)) > -0.5)
+			{
+				CountOffsetNeg[i] = target.count;
+				flag[i] = true;
+				begin_pjs[i] = target.model->motionPool()[i].mp();
+			}
+
+		}
+	}
+
+
+
+	double TransMatrix[4][4];
+	for (int i = 0;i < 4;i++)
+		for (int j = 0;j < 4;j++)
+			TransMatrix[i][j] = begin_pm[4 * i + j];
+
+	double n[3] = { TransMatrix[0][0], TransMatrix[0][1], TransMatrix[0][2] };
+	double o[3] = { TransMatrix[1][0], TransMatrix[1][1], TransMatrix[1][2] };
+	double a[3] = { TransMatrix[2][0], TransMatrix[2][1], TransMatrix[2][2] };
 	
 
 
@@ -3587,10 +3640,10 @@ auto ForceDirect::executeRT(PlanTarget &target)->int
 
     if (target.count % 300 == 0)
     {
-        cout<<ft[2]<<"****"<<FT[2]<<"****"<<ft[5]<<"****"<<JoinTau[1]<<"****"<<stateTor1[2][0]<<std::endl;
+        cout<<ft[2]<<"****"<<FT[2]<<"****"<<step_pjs[1]<<"****"<<JoinTau[1]<<"****"<<stateTor1[2][0]<<std::endl;
     }
 
-    lout << ft[2] << ",";lout << FT[2] << ",";
+    lout << ft[2] << ",";lout << FT[2] << ",";lout<<step_pjs[1] << ",";
 
 	double pa[6] = { 0 }, ta[6] = { 0 };
 	for (int i = 0; i < 6; i++)
@@ -3647,7 +3700,7 @@ auto ForceDirect::executeRT(PlanTarget &target)->int
     }
 
 
-    return 1500000 - target.count;
+    return 35000 - target.count;
 
 }
 
