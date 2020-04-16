6d008b37174393343279f2b38ddf0e5a1297ef7e
KevinLiao888 <lnch-628120@163.com>
2020-04-16 10:51:46 +0800

    huaerkang

--- src/kaanh/main.cpp
+++ src/kaanh/main.cpp
@@ -38,6 +38,7 @@ int main(int argc, char *argv[])
 
 
 	//生成kaanh.xml文档
+    /*
     //-------for rokae robot begin
     cs.resetController(kaanhconfig::createControllerRokaeXB4().release());
     cs.resetModel(kaanhconfig::createModelRokae().release());
@@ -51,7 +52,7 @@ int main(int argc, char *argv[])
     cs.model().loadXmlFile(modelxmlpath.string().c_str());
     cs.saveXmlFile(xmlpath.string().c_str());
     //-------for rokae robot end// 
-
+    */
     
     /*
 	auto ret_load = cal.calculateExpression("pose({1,2,3,4,5,6,7})");
